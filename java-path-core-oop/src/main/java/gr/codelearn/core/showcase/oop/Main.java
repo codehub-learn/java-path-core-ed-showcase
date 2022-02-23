2fc7c0175f68d157541fd782ed979807965900ec
IoannisKlian <giannisklian@gmail.com>
2022-02-23 12:27:23 +0200

    Add main class examples for OOP

--- java-path-core-oop/src/main/java/gr/codelearn/core/showcase/oop/Main.java
+++ java-path-core-oop/src/main/java/gr/codelearn/core/showcase/oop/Main.java
@@ -1,7 +1,23 @@
 package gr.codelearn.core.showcase.oop;
 
+import gr.codelearn.core.showcase.oop.model.Laptop;
+import java.math.BigDecimal;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("OOP module examples");
-	}
+
+    public static void main(String[] args) {
+        Laptop laptop = new Laptop();
+        laptop.setSerial("LP50X22");
+        laptop.setName("ThinkBook");
+        laptop.setPrice(new BigDecimal("700.50"));
+     
+        System.out.println("Laptop serial: "+ laptop.getSerial());
+        System.out.println("Laptop name: "+ laptop.getName());
+        System.out.println("Laptop price: "+ laptop.getPrice());
+        System.out.println(laptop);
+        
+        Laptop laptop2 = new Laptop(4,"LP30x21","Inspitation",new BigDecimal("200"));
+        System.out.println(laptop2);
+        
+    }
 }
