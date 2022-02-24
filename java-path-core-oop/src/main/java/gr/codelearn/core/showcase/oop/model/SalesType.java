9477473073889455f19d6ef979d74c7091610222
dimitris <iracleous@gmail.com>
2022-02-24 13:07:05 +0200

    main

---
+++ java-path-core-oop/src/main/java/gr/codelearn/core/showcase/oop/model/SalesType.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package gr.codelearn.core.showcase.oop.model;
+
+/**
+ *
+ * @author iracl
+ */
+public enum SalesType {
+    
+}
